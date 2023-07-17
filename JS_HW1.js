13d6aefc97a7eacecfbd9f0ff064eb87f88ff0cb
DariaMartinovskaya <136268913+DariaMartinovskaya@users.noreply.github.com>
2023-07-17 20:32:41 +0300

    Update JS_HW1.js

--- JS_HW1.js
+++ JS_HW1.js
@@ -63,12 +63,30 @@
  // 33. Иначе выводите “Technical work”.
  if (age_1<age_2){
      console.log("You don't have access cause your age is "+age_1+" It's less then ")
- } else if (age_1 >=  age_2){
+ } else if (age_1 >=  age_2 && age_1 <  age_3){
      console.log ("Welcome!")
- } else if (age_1 <  age_3){
-     console.log ("Welcom!")
  } else if (age_1  > age_3){
      console.log ("Keep calm and look Culture channel")
  } else {
      console.log ("Technical work")
  }
+
+// 1*: Преобразовать написанный код в 26-33 пунктах в функцию, принимающую на вход возраст.  
+// Пример: const checkAge = function(age) {  
+// Ваши преобразования  
+// }  
+// Вывести в консоль результат работы функции с возрастами 17, 18, 61  
+function CheckAge(age){
+if (age<18){
+return ('You do not have access cause your age is '+age+' It is less then')
+} else if (age >= 18 && age < 60) {
+    return ('Welcome!')
+} else if (age  > 60) {
+    return ('Keep calm and look Culture channel')
+} else {
+    return ('Technical work')
+}
+}
+console.log(CheckAge(17))
+console.log(CheckAge(18))
+console.log(CheckAge(61))

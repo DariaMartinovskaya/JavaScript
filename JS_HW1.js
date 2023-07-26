8a3816bae7a20af52a702af4ca6821ddc8c0215a
DariaMartinovskaya <136268913+DariaMartinovskaya@users.noreply.github.com>
2023-07-26 17:40:41 +0300

    Update JS_HW1.js

--- JS_HW1.js
+++ JS_HW1.js
@@ -1,35 +1,68 @@
-// var item_1 = 5;
-// console.log(item_1)
-// var item_2 = 3;
-// console.log(item_2);
-// var item_3 = item_1+item_2;
-// console.log(item_3);
-// var item_4 = "Yolochka";
-// console.log(item_4);
-// console.log(item_3 + item_4);
-// console.log(item_3 * item_4);
-// var item_5 = item_3;
-// var item_6 =15;
-// var item_6_type = typeof (item_6);
-// console.log('item_6 == '+item_6, 'item_6_type == '+item_6_type);
-// var item_7 = 'item_6';
-// var item_7_type = typeof (item_7);
-// console.log('item_7 == '+item_7, 'item_7_type == '+item_7_type);
+ // 1. Создать переменную “item_1”
+ // 2. Присвоить переменной item_1 цифру 5.
+ // 3. Вывести в консоль item_1.
+ // 4. Создать переменную “item_2”
+ // 5. Присвоить переменной item_2 цифру 3.
+ // 6. Вывести в консоль item_2.
+ // 7. Создать переменную “item_3”
+ // 8. Присвоить переменной item_3 сложение item_1 и item_2.
+ // 9. Вывести в консоль item_3.
+ // 10. Создать переменную “item_4”
+ // 11. Присвоить переменной item_4 строку “Yolochka”
+ // 12. Вывести в консоль item_4.
+ // 13. Вывести в консоль сложение item_3 и item_4.
+ // 14. Вывести в консоль умножение item_3 и item_4.
+ // 15. Создать переменную “item_5”
+ // 16. Присвоить переменной item_5 переменную item_3
+ // 17. Создать переменную item_6.
+ // 18. Создать переменную item_6_type
+ // 19. Присвоить переменной item_6 значение 15
+ // 20. Присвоить переменной item_6_type тип переменной item_6
+ // 21. Вывести в консоль тип данных item_6 в виде ——  “item_6 == ”  item_6,  “item_6_type == ”  item_6_type ——  
+ // 22. Создать переменную item_7 и в ней преобразовать item_6 в String.
+ // 23. Создать переменную item_7_type
+ // 24. Присвоить переменной item_7_type тип переменной item_7
+ // 25. Вывести в консоль тип данных item_7 в виде ——  “item_7 == ”  item_7,  “item_7_type == ”  item_7_type ——  
+ // 26. Создать переменную “age_1” и присвоить ей значение 10
+ // 27. Создать переменную “age_2” и присвоить ей значение 18
+ // 28. Создать переменную “age_3” и присвоить ей значение 60
+ // 29. Создать if в котором будите проверять значение переменной age_1
+ // 30. Если age_1 < age_2, вывести в консоль “You don’t have access cause your age is ” + age_1 + “ It’s less then ”
+ // 31. Если age_1 >=  age_2 и age_1 <  age_3, вывести в консоль “Welcome  !”
+ // 32. Если age_1  > age_3, вывести в консоль “Keep calm and look Culture channel”.
+ // 33. Иначе выводите “Technical work”.
+     
+var item_1 = 5;
+console.log(item_1)
+var item_2 = 3;
+console.log(item_2);
+var item_3 = item_1+item_2;
+console.log(item_3);
+var item_4 = "Yolochka";
+console.log(item_4);
+console.log(item_3 + item_4);
+console.log(item_3 * item_4);
+var item_5 = item_3;
+var item_6 =15;
+var item_6_type = typeof (item_6);
+console.log('item_6 == '+item_6, 'item_6_type == '+item_6_type);
+var item_7 = 'item_6';
+var item_7_type = typeof (item_7);
+console.log('item_7 == '+item_7, 'item_7_type == '+item_7_type);
 
-// const age_1 = 10;
-// const age_2 = 18;
-// const age_3 = 60;
+const age_1 = 10;
+const age_2 = 18;
+const age_3 = 60;
 
-// if (age_1<age_2){
-//     console.log("You don't have access cause your age is "+age_1+" It's less then " + age_2)
-// } else if (age_1 >=  age_2 && age_1 <  age_3){
-//     console.log ("Welcome!")
-// } else if (age_1  > age_3){
-//     console.log ("Keep calm and look Culture channel")
-// } else {
-//     console.log ("Technical work")
-// }
-
+if (age_1<age_2){
+console.log("You don't have access cause your age is "+age_1+" It's less then " + age_2)
+} else if (age_1 >=  age_2 && age_1 <  age_3){
+console.log ("Welcome!")
+} else if (age_1  > age_3){
+console.log ("Keep calm and look Culture channel")
+} else {
+console.log ("Technical work")
+}
 
 // 1*: Преобразовать написанный код в 26-33 пунктах в функцию, принимающую на вход возраст.  
 // Пример: const checkAge = function(age) {  
@@ -37,80 +70,80 @@
 // }  
 // Вывести в консоль результат работы функции с возрастами 17, 18, 61  
 
-// const age_1 = 10;
-// const age_2 = 18;
-// const age_3 = 60;
+const age_1 = 10;
+const age_2 = 18;
+const age_3 = 60;
 
-// const CheckAge = function(age){
-//     if (age<age_2){
-//     return ('You do not have access cause your age is '+age+' It is less then ' + age_2)
-//     } else if (age >= age_2 && age < age_3) {
-//         return ('Welcome!')
-//     } else if (age  > age_3) {
-//         return ('Keep calm and look Culture channel')
-//     } else {
-//         return ('Technical work')
-//     }
-//     }
+const CheckAge = function(age){
+if (age<age_2){
+return ('You do not have access cause your age is '+age+' It is less then ' + age_2)
+} else if (age >= age_2 && age < age_3) {
+return ('Welcome!')
+} else if (age  > age_3) {
+return ('Keep calm and look Culture channel')
+} else {
+return ('Technical work')
+}
+}
     
-//     console.log(CheckAge(17))
-//     console.log(CheckAge(18))
-//     console.log(CheckAge(61))
+console.log(CheckAge(17))
+console.log(CheckAge(18))
+console.log(CheckAge(61))
   
 // 2*:  Преобразовать задание 1* таким образом, чтобы первым делом в функции проверялся тип данных. И если он не Number - кидалась ошибка.  
         
-// const age_1 = 10;
-// const age_2 = 18;
-// const age_3 = 60;
+const age_1 = 10;
+const age_2 = 18;
+const age_3 = 60;
 
-// const CheckAge = function(age) {
-//     if (typeof age == 'number'){
-//     if (age<age_2) {
-//     return ('You do not have access cause your age is '+age+' It is less then ' + age_2)
-//     } else if (age >= age_2 && age < age_3) {
-//         return ('Welcome!')
-//     } else if (age  > age_3) {
-//         return ('Keep calm and look Culture channel')
-//     } else {
-//         return ('Technical work')
-//     } 
-// } else console.log('Not integer value')
-// }
+const CheckAge = function(age) {
+if (typeof age == 'number'){
+if (age<age_2) {
+return ('You do not have access cause your age is '+age+' It is less then ' + age_2)
+} else if (age >= age_2 && age < age_3) {
+return ('Welcome!')
+} else if (age  > age_3) {
+return ('Keep calm and look Culture channel')
+} else {
+return ('Technical work')
+} 
+} else console.log('Not integer value')
+}
     
-// console.log(CheckAge(17))
-// console.log(CheckAge(18))
-// console.log(CheckAge(61))
-// console.log(CheckAge('test'))
+console.log(CheckAge(17))
+console.log(CheckAge(18))
+console.log(CheckAge(61))
+console.log(CheckAge('test'))
   
 // 3**: Преобразовать 2* таким образом, чтобы значение '2' (строка в которой лежит ТОЛЬКО ЦИФРА) пропускалось, преобразовываясь в number  
 
-// const age_1 = 10;
-// const age_2 = 18;
-// const age_3 = 60;
+const age_1 = 10;
+const age_2 = 18;
+const age_3 = 60;
 
-// const CheckAge = function(age) {
+const CheckAge = function(age) {
 
-//     age = Number(age)
+age = Number(age)
 
-//     if(!isNaN(age)) {
+if(!isNaN(age)) {
 
-//     if (age<age_2) {
-//     return ('You do not have access cause your age is '+age+' It is less then ' + age_2)
-//     } else if (age >= age_2 && age < age_3) {
-//         return ('Welcome!')
-//     } else if (age  > age_3) {
-//         return ('Keep calm and look Culture channel')
-//     } else {
-//         return ('Technical work')
-//     } 
-// } else console.log('Not integer value')}
+if (age<age_2) {
+return ('You do not have access cause your age is '+age+' It is less then ' + age_2)
+} else if (age >= age_2 && age < age_3) {
+return ('Welcome!')
+} else if (age  > age_3) {
+return ('Keep calm and look Culture channel')
+} else {
+return ('Technical work')
+} 
+} else console.log('Not integer value')}
     
-// console.log(CheckAge(17))
-// console.log(CheckAge(18))
-// console.log(CheckAge(61))
-// console.log(CheckAge('2'))
-// console.log(CheckAge('test'))
-// console.log(CheckAge('22test'))
+console.log(CheckAge(17))
+console.log(CheckAge(18))
+console.log(CheckAge(61))
+console.log(CheckAge('2'))
+console.log(CheckAge('test'))
+console.log(CheckAge('22test'))
 
 // 4***:
 // Преобразовать задание 3* таким образом, чтобы возраст вводится используя функцию prompt в привязанной верстке

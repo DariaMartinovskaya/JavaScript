f861feb6fdcc97f68b793b6d1813ffcb7274ea76
DariaMartinovskaya <136268913+DariaMartinovskaya@users.noreply.github.com>
2024-04-05 23:45:29 +0100

    Update JS_HW1.js

--- JS_HW1.js
+++ JS_HW1.js
@@ -1,36 +1,36 @@
- // 1. Создать переменную “item_1”
- // 2. Присвоить переменной item_1 цифру 5.
- // 3. Вывести в консоль item_1.
- // 4. Создать переменную “item_2”
- // 5. Присвоить переменной item_2 цифру 3.
- // 6. Вывести в консоль item_2.
- // 7. Создать переменную “item_3”
- // 8. Присвоить переменной item_3 сложение item_1 и item_2.
- // 9. Вывести в консоль item_3.
- // 10. Создать переменную “item_4”
- // 11. Присвоить переменной item_4 строку “Yolochka”
- // 12. Вывести в консоль item_4.
- // 13. Вывести в консоль сложение item_3 и item_4.
- // 14. Вывести в консоль умножение item_3 и item_4.
- // 15. Создать переменную “item_5”
- // 16. Присвоить переменной item_5 переменную item_3
- // 17. Создать переменную item_6.
- // 18. Создать переменную item_6_type
- // 19. Присвоить переменной item_6 значение 15
- // 20. Присвоить переменной item_6_type тип переменной item_6
- // 21. Вывести в консоль тип данных item_6 в виде ——  “item_6 == ”  item_6,  “item_6_type == ”  item_6_type ——  
- // 22. Создать переменную item_7 и в ней преобразовать item_6 в String.
- // 23. Создать переменную item_7_type
- // 24. Присвоить переменной item_7_type тип переменной item_7
- // 25. Вывести в консоль тип данных item_7 в виде ——  “item_7 == ”  item_7,  “item_7_type == ”  item_7_type ——  
- // 26. Создать переменную “age_1” и присвоить ей значение 10
- // 27. Создать переменную “age_2” и присвоить ей значение 18
- // 28. Создать переменную “age_3” и присвоить ей значение 60
- // 29. Создать if в котором будите проверять значение переменной age_1
- // 30. Если age_1 < age_2, вывести в консоль “You don’t have access cause your age is ” + age_1 + “ It’s less then ”
- // 31. Если age_1 >=  age_2 и age_1 <  age_3, вывести в консоль “Welcome  !”
- // 32. Если age_1  > age_3, вывести в консоль “Keep calm and look Culture channel”.
- // 33. Иначе выводите “Technical work”.
+ // 1. Create the variable “item_1”
+ // 2. Assign the variable item_1 the number 5.
+ // 3. Print into console item_1.
+ // 4. Create the variable “item_2”
+ // 5. Assign the variable item_2 the number 3.
+ // 6. Print into console item_2.
+ // 7. Create the variable “item_3”
+ // 8. Assign the variable item_3 sum of item_1 and item_2.
+ // 9. Print into console item_3.
+ // 10. Create the variable “item_4”
+ // 11. Assign the variable item_4 the string “Yolochka”
+ // 12. Print into console item_4.
+ // 13. Print into console the sum of item_3 and item_4.
+ // 14. Print into console multiplication ща item_3 and item_4.
+ // 15. Create the variable “item_5”
+ // 16. Assign the variable item_5 the variable item_3
+ // 17. Create the variable item_6.
+ // 18. Create the variable item_6_type
+ // 19. Assign the variable item_6 the number 15
+ // 20. Assign the variable item_6_type type of the variable item_6
+ // 21. Print into console the data type of item_6 in the form of  ——  “item_6 == ”  item_6,  “item_6_type == ”  item_6_type ——  
+ // 22. Create the variable item_7 and cinvert item_6 into String in it.
+ // 23. Create the variable item_7_type
+ // 24. Assign the variable item_7_type type of the variable item_7
+ // 25. Print into console the data type of item_7 in the form of ——  “item_7 == ”  item_7,  “item_7_type == ”  item_7_type ——  
+ // 26. Create the variable “age_1” and assight the number 10 to it
+ // 27. Create the variable “age_2” and assight the number 18 to it
+ // 28. Create the variable “age_3” and assight the number 60 to it
+ // 29. Create if in which the value of the variable age_1 will be checked
+ // 30. If age_1 < age_2, print into the console “You don’t have access cause your age is ” + age_1 + “ It’s less then ”
+ // 31. If age_1 >=  age_2 and age_1 <  age_3, print into the console “Welcome  !”
+ // 32. If age_1  > age_3, print into the console “Keep calm and look Culture channel”.
+ // 33. Otherwise print into the console “Technical work”.
      
 var item_1 = 5;
 console.log(item_1)
